797a539760a7eb279e6d2ece787a29a497928b33
Programlink <snr.webster@gmail.com>
2025-05-07 00:23:37 +0300

    First email in raw binary not parsed

--- omnimail-backend/app/core/email_fetcher.py
+++ omnimail-backend/app/core/email_fetcher.py
@@ -8,5 +8,6 @@ def fetch_emails(username, password, imap_server="imap.gmail.com"):
     mail_conn = imaplib.IMAP4_SSL(imap_server)
     mail_conn.login(username,password)
     mail_conn.select("INBOX")
-    
-    return mail_conn.search(None,"all")
+    status,inbox_emails = mail_conn.search(None,"all")
+    return mail_conn.fetch("1","(RFC822)")
+            
